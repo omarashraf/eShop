e2bf9ef0c1dcfacbb09d9a190078dcf3cd8d6e9c
Omar <o.abouzaid93@gmail.com>
2015-10-06 13:48:34 +0200

    cart styling is done

--- cart.php
+++ cart.php
@@ -10,6 +10,7 @@
   <script src="js/vendor/placeholder.js"></script>
   <script src="js/vendor/fastclick.js"></script>
   <script src="js/foundation.min.js"></script>
+  <script src="typed.js-master/js/typed.js"></script>
 </head>
 
 <body>
@@ -65,38 +66,76 @@
 		</div>
 		<br>
 
-  <div style="float: right;">
+  <!--<div style="float: right;">
   		<a href="productshome.php" class="button success">Add items</a>
-  </div>
+  </div>-->
+
+  
 
 	<script>
     $(document).foundation();
   </script>
 
-</body>
-</html>
+
+
 <?php
-	echo 'My cart' . "<br>";
 	//session_start();
 	if (isset($_SESSION['cart'])) {
 		$temp_arr = array();
 		$temp_arr = $_SESSION['cart'];
-		for ($i = 0; $i < count($_SESSION['cart']); $i++) {
-			echo $temp_arr[$i] . "<br>";
-			$products_display_sql = "SELECT * FROM products WHERE id = '" . $temp_arr[$i] . "'";
-			$res = mysql_query($products_display_sql);
-
-			if (mysql_num_rows($res) > 0) {
-				while($temp = mysql_fetch_assoc($res)) {
-					echo "Product name: " . $temp['pName'] . ", description: " . $temp['description'] . ", and price: " .$temp['price'];
-					echo " <a href='removeFromCart.php?id=" . $i . "'>Remove</a>" . "<br>";
-					break;
+		if (count($temp_arr) > 0) {
+			echo '<div class="row">
+				  	<h1 style="text-align: center;">My Cart</h1>
+				  	<div class="small-12 small-centered columns">
+				  		<table width="100%">
+				  			<thead>
+				  				<tr>
+				  					<th>Name</th>
+				  					<th>Description</th>
+				  					<th>Price</th>
+				  					<th></th>
+				  				</tr>
+				  			</thead>
+				   </div>';
+			for ($i = 0; $i < count($_SESSION['cart']); $i++) {
+				//echo $temp_arr[$i] . "<br>";
+				$products_display_sql = "SELECT * FROM products WHERE id = '" . $temp_arr[$i] . "'";
+				$res = mysql_query($products_display_sql);
+				echo '';
+				if (mysql_num_rows($res) > 0) {
+					while($temp = mysql_fetch_assoc($res)) {
+						echo '<tr>
+					  				<th>' . $temp['pName'] . '</th>
+					  				<th>' . $temp['description'] . '</th>
+					  				<th>' . $temp['price'] . '</th>
+					  				<th><a href=\'removeFromCart.php?id=" . $i . "\'>Remove</a></th>
+					  			</tr>';
+						break;
+					}
 				}
 			}
+			echo '</table>
+				</div>';
+		}
+		else {
+			echo '<div class="element" style="text-align: center; font-size: 40px; margin-top: 300px;"></div>' . '<br>';
+			echo '<div style="text-align: center;"><a href="productshome.php" class="button alert">Add now</a></div>';
+			echo '<script>
+					$(document).foundation();
+					$(function(){
+					    $(".element").typed({
+					      strings: ["Your cart is now empty. You can add items throught our products page."],
+					      typeSpeed: 0
+					    });
+					});
+				</script>';
 		}
 	}
 
 ?>
+		
+</body>
+</html>
   <!--<form name="checkoutForm" action="checkout.php" method="post">
     <input type='hidden' name='checkedout' id='checkedout' value='1'></input>
     <button type="submit" class="button success">Checkout</button>
@@ -104,17 +143,24 @@
 
   
 <?php
-
-	if (isset($_SESSION['loggedin']) && isset($_SESSION['loggedin_password'])) {
-		echo "<a href=\"checkout.php\" onclick=\"confirm()\" class=\"button success\">Checkout</a>";
-		echo "<script>
-						function confirm() {
-						    alert(\"You have successfully purchased the items!\");
-						}
-					</script>";
+	$temp_arr = array();
+	
+	$temp_arr = $_SESSION['cart'];
+	if (count($temp_arr) != 0) {
+		if (isset($_SESSION['loggedin']) && isset($_SESSION['loggedin_password'])) {
+			echo "<a href=\"checkout.php\" onclick=\"confirm()\" class=\"button success\" style='float: right; margin-right: 35px;'>Checkout</a>";
+			echo "<a href=\"productshome.php\" class=\"button alert\" style='float: right; margin-right: 5px;'>Add items</a>";
+			echo "<script>
+							function confirm() {
+							    alert(\"You have successfully purchased the items!\");
+							}
+						</script>";
+		}
+		else {
+			echo "<a href=\"checkout.php\" class=\"button success\" style='float: right;'>Checkout</a>";
+			echo "<a href=\"productshome.php\" class=\"button alert\" style='float: right; margin-right: 5px;'>Add items</a>";
+		}
 	}
-	else {
-		echo "<a href=\"checkout.php\" class=\"button success\">Checkout</a>";
-	}
+	
 ?>
 
